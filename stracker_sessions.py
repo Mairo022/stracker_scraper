a4d12d7dbf758149e35d37e240bb9b24ec94e0e0
Mairo022 <mairo022@outlook.com>
2023-12-07 16:42:47 +0200

    Workflow & get data

--- stracker_sessions.py
+++ stracker_sessions.py
@@ -1,11 +1,12 @@
 from time import sleep
 from selenium import webdriver
-from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-URL = "https://stracker.sdrive.club/sessionstat"
+BASE_URL = ""
+SESSIONS_URL = f"{BASE_URL}/sessionstat"
+
 
 def main():
     options = webdriver.ChromeOptions()
@@ -13,36 +14,68 @@ def main():
     options.page_load_strategy = 'eager'
 
     browser = webdriver.Chrome(options=options)
+    handleSessions(browser)
+    print("Program has finished")
 
-    browser.get(URL)
-    pagination = (WebDriverWait(browser, 10)
-                  .until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".pagination > li"))))
 
-    page_last = int(pagination[-3].text)
+def handleSessions(browser):
+    browser.get(SESSIONS_URL)
 
-    for page_current in range(0, page_last-1):
+    for page_current in range(0, 50):
         sessionsPage(browser)
+        browser.get(SESSIONS_URL + '?page=' + str(page_current + 1))
 
-        browser.get(URL + '?page=' + str(page_current+1))
         pagination = (WebDriverWait(browser, 10)
                       .until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".pagination > li"))))
 
         page_last = int(pagination[-3].text)
 
+        if page_current + 1 == page_last:
+            break
+
 
 def sessionsPage(browser):
-    sessions = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, 'tbody')))
-    sessions_len = len(sessions)
+    sessions = (WebDriverWait(browser, 10)
+                .until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'tbody > tr'))))
 
-    for i in range(0, sessions_len):
-        break
-        # sessionPage(browser)
+    sessions_links = [session.get_attribute("href") for session in sessions]
 
-    print("Program has finished")
+    for session_link in sessions_links:
+        sessionPage(browser, f"{BASE_URL}/{session_link}")
+        sleep(1)
+
+    browser.execute_script("window.history.go(-1)")
 
 
-#def sessionPage(browser):
+def sessionPage(browser, url):
+    browser.get(url)
+    extractAndWriteSessionData(browser)
+    sleep(1)
+    browser.execute_script("window.history.go(-1)")
 
+
+def extractAndWriteSessionData(browser):
+    tables = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, "tbody")))
+    session_overview = tables[0].find_elements(By.TAG_NAME, "td")
+    session_details = tables[1].find_elements(By.TAG_NAME, "tr")
+
+    track = session_overview[0].text
+    session = session_overview[1].text
+    duration = session_overview[2].text
+    datetime = session_overview[3].text
+
+    for row in session_details:
+        items = row.find_elements(By.TAG_NAME, "td")
+        position = items[0].text
+        driver = items[1].text
+        car = items[2].text
+        fastest_lap = items[3].text
+        gap_to_first = items[4].text
+        laps = items[5].text
+        cuts = items[6].text
+        crashes = items[7].text
+
+    # TODO: Save data
 
 
 main()
